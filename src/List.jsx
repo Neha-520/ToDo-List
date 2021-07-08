eac108355b41ca51b11a0251fba0e397af847970
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-07-09 00:08:22 +0530

    minor

--- src/List.jsx
+++ src/List.jsx
@@ -5,10 +5,10 @@ const List = (props)=>
 {
     
  return (<>
- <div >
+ <div className="showItems">
  <li className ="eachItem" >
  <h3>{props.text}</h3>
- <div className="todo-btn center_div">
+ <div className="todo-btn ">
  
  <i className="bi bi-pencil-square" style={{fontSize: '2rem', marginLeft: '25px',cursor:'pointer'}}  onClick={() =>{props.editItems(props.id);}}></i>
  <i className="bi bi-trash-fill" style={{fontSize: '2rem', marginLeft: '25px',paddingRight:'14px',cursor:'pointer' }}  onClick={() =>{props.onSelect(props.id);}}></i>
